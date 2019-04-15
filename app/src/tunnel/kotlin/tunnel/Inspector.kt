b0d1096619f2858d2cc3aa930c712d0a1003f857
Antonio Galea <antonio.galea@gmail.com>
2019-04-16 01:16:26 +0200

    Inspector, refactoring, support for IPv4, IPv6 and ICMP

--- app/src/tunnel/kotlin/tunnel/Inspector.kt
+++ app/src/tunnel/kotlin/tunnel/Inspector.kt
@@ -6,33 +6,53 @@ import java.io.File
 
 internal class Inspector {
     fun inspect(ktx: Kontext, packet: IpPacket) {
-        var payload = packet.payload
         var protocol = ""
         var signature = ""
-        if (packet.payload is UdpPacket) {
-            protocol = "udp"
-            payload = packet.payload as UdpPacket
-            signature = "%s %s".format(
-                toHex(packet.header.srcAddr.getAddress(), payload.header.srcPort.valueAsInt()),
-                toHex(packet.header.dstAddr.getAddress(), payload.header.dstPort.valueAsInt())
-            )
-        } else if (packet.payload is TcpPacket) {
-            protocol = "tcp"
-            payload = packet.payload as TcpPacket
-            signature = "%s %s".format(
-                toHex(packet.header.srcAddr.getAddress(), payload.header.srcPort.valueAsInt()),
-                toHex(packet.header.dstAddr.getAddress(), payload.header.dstPort.valueAsInt())
-            )
+        when (packet.payload) {
+            is UdpPacket -> {
+                protocol = "udp"
+                if (packet is IpV6Packet) protocol += "6"
+                val payload = packet.payload as UdpPacket
+                signature = "%s %s".format(
+                    toHex(packet.header.srcAddr.getAddress(), payload.header.srcPort.valueAsInt()),
+                    toHex(packet.header.dstAddr.getAddress(), payload.header.dstPort.valueAsInt())
+                )
+            }
+            is TcpPacket -> {
+                protocol = "tcp"
+                if (packet is IpV6Packet) protocol += "6"
+                val payload = packet.payload as TcpPacket
+                signature = "%s %s".format(
+                    toHex(packet.header.srcAddr.getAddress(), payload.header.srcPort.valueAsInt()),
+                    toHex(packet.header.dstAddr.getAddress(), payload.header.dstPort.valueAsInt())
+                )
+            }
+            is IcmpV4CommonPacket -> {
+                protocol = "icmp"
+                val payload = packet.payload.getRawData()
+                val icmp_id = payload[5] * 256 + payload[4]
+                signature = "%s %s".format(
+                    toHex(packet.header.srcAddr.getAddress(), icmp_id),
+                    toHex(packet.header.dstAddr.getAddress(), 0)
+                )
+            }
+            is IcmpV6CommonPacket -> {
+                protocol = "icmp6"
+                val payload = packet.payload.getRawData()
+                val icmp_id = payload[5] * 256 + payload[4]
+                signature = "%s %s".format(
+                    toHex(packet.header.srcAddr.getAddress(), icmp_id),
+                    toHex(packet.header.dstAddr.getAddress(), 0)
+                )
+            }
+            else -> return
         }
 
-        if (protocol != "") {
-            if (packet is IpV6Packet) protocol = protocol + "6"
-            File("/proc/net/%s".format(protocol)).useLines {
-                lines -> lines.forEach { line ->
-                    if (signature in line) {
-                        val tokens = line.split(" +".toRegex())
-                        ktx.v("PACKET %s: src=%s, dst=%s, uid=%s".format(protocol.toUpperCase(), tokens[2], tokens[3], tokens[8]))
-                    }
+        File("/proc/net/%s".format(protocol)).useLines {
+            lines -> lines.forEach { line ->
+                if (signature in line) {
+                    val tokens = line.split(" +".toRegex())
+                    ktx.v("PACKET %s: src=%s, dst=%s, uid=%s".format(protocol.toUpperCase(), tokens[2], tokens[3], tokens[8]))
                 }
             }
         }
