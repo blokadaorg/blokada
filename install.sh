cb6f1f188cf9d384f5c4bffb20a7712c2b7f256a
kar <hi@kar.gs>
2019-06-16 21:24:59 +0200

    make it compile for aarch64

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 JNI_LIBS=../app/src/main/jniLibs
 
+export PATH=$PATH:$NDK_STANDALONE/arm64/bin
+
 cd boringtun
-#cargo build --lib --release --target aarch64-linux-android
-cargo build --lib --release --target armv7-linux-androideabi
-#cargo build --lib --release --target i686-linux-android
 
 rm -rf $JNI_LIBS
 mkdir $JNI_LIBS
@@ -12,9 +11,21 @@ mkdir $JNI_LIBS/arm64-v8a
 mkdir $JNI_LIBS/armeabi-v7a
 mkdir $JNI_LIBS/x86
 
-#cp target/aarch64-linux-android/release/libboringtun.so $JNI_LIBS/arm64-v8a/libboringtun.so
+echo "Building for armv7..."
+cargo build --lib --release --target armv7-linux-androideabi
 cp target/armv7-linux-androideabi/release/libboringtun.so $JNI_LIBS/armeabi-v7a/libboringtun.so
-#cp target/i686-linux-android/release/libboringtun.so $JNI_LIBS/x86/libboringtun.so
+
+echo "Building for aarch64..."
+export CC=aarch64-linux-android-gcc
+export CXX=aarch64-linux-android-g++
+cargo build --lib --release --target aarch64-linux-android
+cp target/aarch64-linux-android/release/libboringtun.so $JNI_LIBS/arm64-v8a/libboringtun.so
+
+echo "Building for i686..."
+export CC=i686-linux-android-gcc
+export CXX=i686-linux-android-g++
+cargo build --lib --release --target i686-linux-android
+cp target/i686-linux-android/release/libboringtun.so $JNI_LIBS/x86/libboringtun.so
 
 cd ../
 ./gradlew iAHD
