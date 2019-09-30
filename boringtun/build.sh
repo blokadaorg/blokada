c1bf3dcdb38333ee64904051bf5af6ef20be9817
kar <hi@kar.gs>
2019-09-30 11:02:40 +0200

    update build script to support x86_64

--- boringtun/build.sh
+++ boringtun/build.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
 JNI_LIBS=../app/src/tun-blocka/jniLibs
 
+export PATH=$PATH:$NDK_STANDALONE/arm/bin
 export PATH=$PATH:$NDK_STANDALONE/arm64/bin
 export PATH=$PATH:$NDK_STANDALONE/x86/bin
+export PATH=$PATH:$NDK_STANDALONE/x86_64/bin
 
 rm -rf $JNI_LIBS
 mkdir $JNI_LIBS
-mkdir $JNI_LIBS/arm64-v8a
 mkdir $JNI_LIBS/armeabi-v7a
+mkdir $JNI_LIBS/arm64-v8a
 mkdir $JNI_LIBS/x86
+mkdir $JNI_LIBS/x86_64
 
 echo "Building for armv7..."
+export CC=arm-linux-android-gcc
+export CXX=arm-linux-android-g++
 cargo build --lib --release --target armv7-linux-androideabi
 cp target/armv7-linux-androideabi/release/libboringtun.so $JNI_LIBS/armeabi-v7a/libboringtun.so
 
@@ -25,3 +30,9 @@ export CC=i686-linux-android-gcc
 export CXX=i686-linux-android-g++
 cargo build --lib --release --target i686-linux-android
 cp target/i686-linux-android/release/libboringtun.so $JNI_LIBS/x86/libboringtun.so
+
+echo "Building for v86_64..."
+export CC=x86_64-linux-android-gcc
+export CXX=x86_64-linux-android-g++
+cargo build --lib --release --target x86_64-linux-android
+cp target/x86_64-linux-android/release/libboringtun.so $JNI_LIBS/x86_64/libboringtun.so
