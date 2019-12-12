fb81ebefcbd599715fb19a690eef01c6a874dae9
De_Coder <de.coder@gmx.de>
2019-12-12 22:46:59 +0100

    removed test-data

--- app/src/ui-blokada/kotlin/gs/property/Repo.kt
+++ app/src/ui-blokada/kotlin/gs/property/Repo.kt
@@ -62,13 +62,10 @@ class RepoImpl(
         try {
             ktx.v("repo downloaded")
             val repoData = loadGzip(openUrl(repoURL, fetchTimeout))
-            //val jsonRepo = JSONObject(repoData)
-            //REMOVE!!!!!!!!!!!!!!!!
-            val jsonRepo = "{\"contentPath\":\"https://blokada.org/api/v4/content\",\"locales\":[\"en\",\"pl_PL\",\"hu_HU\",\"ru_RU\",\"es_ES\",\"hi_IN\",\"tr_TR\",\"fr_FR\",\"ms_MY\",\"uk_UA\",\"de_DE\",\"cs_CZ\",\"it_IT\",\"pt_BR\",\"nb_NO\",\"zh_TW\",\"id_ID\",\"zh_CN\",\"bg_BG\",\"lv_LV\",\"ca_ES\",\"pt_PT\",\"ar_SA\",\"sk_SK\",\"iw_IL\",\"zu_Z\"],\"newestVersionCode\":404000002,\"newestVersionName\":\"4.4.2\",\"downloadLinks\":[\"https://github.com/blokadaorg/blokada/releases/download/4.4.2/blokada-v4.4.2.apk\",\"https://bitbucket.org/blokada/blokada/downloads/blokada-v4.4.2.apk\"]}"
             lastRefreshMillis %= time.now()
 
             val gson = Gson()
-            val jsonRepoContent = gson.fromJson(jsonRepo, JsonRepoContent::class.java)
+            val jsonRepoContent = gson.fromJson(repoData, JsonRepoContent::class.java)
             RepoContent(
                     contentPath = URL(jsonRepoContent.contentPath),
                     locales = jsonRepoContent.locales.map {
