15e4a73196d95aa980b6f40eee1c6420e4bcef61
De_Coder <de.coder@gmx.de>
2020-08-04 20:38:51 +0200

    add cname setting slot to adblocking settings

--- app/src/ui-blokada/kotlin/core/bits/menu/adblocking/Sections.kt
+++ app/src/ui-blokada/kotlin/core/bits/menu/adblocking/Sections.kt
@@ -94,6 +94,7 @@ private fun createAdblockingSettings(ktx: AndroidKontext): NamedViewBinder {
                     LabelVB(ktx, label = R.string.menu_host_adblocking_features.res()),
                     WildcardVB(ktx, onTap = defaultOnTap),
                     SmartListVB(ktx, onTap = defaultOnTap),
+                    CnameBlockingVB(ktx, onTap = defaultOnTap),
                     LabelVB(ktx, label = R.string.menu_host_adblocking_settings.res()),
                     LoggerVB(ktx, onTap = defaultOnTap),
                     ResetCounterVB(ktx, onTap = defaultOnTap),
