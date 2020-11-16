9ae57c2f22f49b242468392d0913cf74b0550a4d
kar <hi@kar.gs>
2020-11-16 11:08:38 +0100

    add sync script

---
+++ sync-translations.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Syncing strings (will push)"
+
+cd translate/scripts
+git checkout master
+git pull
+hash=$(git rev-parse --short HEAD)
+commit="translate: sync strings to: $hash"
+
+echo $commit
+
+./translate.py -a android5
+
+cd ../../
+
+git commit -am "$commit"
+git push
+
+echo "Done"
