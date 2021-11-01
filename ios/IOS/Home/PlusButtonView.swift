1a18d1d16f7df10b87ad69bd3e358a0cd44ff1ac
kar <hi@kar.gs>
2021-11-01 16:35:14 +0100

    ios: fix plus button wrong start padding

--- ios/IOS/Home/PlusButtonView.swift
+++ ios/IOS/Home/PlusButtonView.swift
@@ -99,7 +99,7 @@ struct PlusButtonView: View {
         .frame(height: 44)
         .padding([.bottom, .leading, .trailing])
         .transition(.slide)
-        .offset(y: self.vm.mainSwitch && !self.vm.isPaused ? 0 : 64)
+        .offset(y: self.vm.mainSwitch && !self.vm.isPaused ? 8 : 69)
         .animation(
             Animation.easeInOut(duration: 0.6).repeatCount(1)
         )
