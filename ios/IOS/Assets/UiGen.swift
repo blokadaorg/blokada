b5799fe5be6d154dcd6c6f85f7049924759f575e
kar <hi@kar.gs>
2021-11-22 17:08:44 +0100

    translate: sync strings to: 368f0ef90

--- ios/IOS/Assets/UiGen.swift
+++ ios/IOS/Assets/UiGen.swift
@@ -900,6 +900,10 @@ internal enum L10n {
   internal static func paymentSubscriptionPerYear(_ p1: Any) -> String {
     return L10n.tr("Ui", "payment subscription per year", String(describing: p1))
   }
+  /// then %@ per year
+  internal static func paymentSubscriptionPerYearThen(_ p1: Any) -> String {
+    return L10n.tr("Ui", "payment subscription per year then", String(describing: p1))
+  }
   /// Your account is now active! You may now use all Blokada Plus features on all your devices.
   internal static let paymentSuccessDesc = L10n.tr("Ui", "payment success desc")
   /// Thanks!
