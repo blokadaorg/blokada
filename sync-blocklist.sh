c081c22858b511a43eb7df97f29f30c2685f314d
kar <hi@kar.gs>
2022-01-25 08:19:13 +0000

    update sync blocklist script path

--- sync-blocklist.sh
+++ sync-blocklist.sh
@@ -2,8 +2,8 @@
 
 echo "Syncing default blocklist"
 
-cp ../landing-github-pages/mirror/v5/oisd/light/hosts.txt ios/Engine/hosts.txt
-cp ios/Engine/hosts.txt android5/app/src/main/assets/default_blocklist
+cp ../landing-github-pages/mirror/v5/oisd/light/hosts.txt ios/BlockaEngine/hosts.txt
+cp ios/BlockaEngine/hosts.txt android5/app/src/main/assets/default_blocklist
 cd android5/app/src/main/assets/
 rm default_blocklist.zip
 zip -e default_blocklist.zip default_blocklist
