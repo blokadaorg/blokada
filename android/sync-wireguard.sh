fb2c992cad49dcd248cd8ddfbef5e913138797d2
kar <hi@kar.gs>
2023-01-06 12:06:05 +0100

    make sync-wireguard.sh script

---
+++ android/sync-wireguard.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#
+# This file is part of Blokada.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright © 2023 Blocka AB. All rights reserved.
+#
+# @author Karol Gusak (karol@blocka.net)
+#
+
+echo "Building wireguard-android..."
+
+cd wireguard-android
+hash=$(git describe --abbrev=4 --always --tags --dirty)
+commit="sync: update wireguard-android to: $hash"
+
+echo $commit
+
+#./gradlew tunnel:clean
+./gradlew tunnel:build
+cp tunnel/build/outputs/aar/tunnel-release.aar ../app/wireguard-android/lib/wg-tunnel.aar
+
+git commit -am "$commit"
+
+echo "Done."
