6da2a446ab6f458ceeabd73091d1219e37fa0395
kar <hi@kar.gs>
2023-01-06 10:29:16 +0100

    update build script to not recompile every time
    Uses a simple repo versioning to see if was built for the current head. Will work
    for the xcode ios build, but it wont work for dirty changes (always rebuild)

--- common/build.for.ios.sh
+++ common/build.for.ios.sh
@@ -2,6 +2,27 @@
 
 set -e
 
-flutter build ios-framework --output=build/ios-framework --no-debug --no-profile
+echo "Building common..."
+
+version=$(git describe --abbrev=4 --always --tags --dirty)
+
+built=false
+if [ -e build/.version ]; then
+  built_version=$(<build/.version)
+  if [[ "$version" = *-dirty ]]; then
+    built=false # always reubild dirty head
+  elif [[ "$version" == "$built_version" ]]; then
+    built=true
+  else
+    built=false
+  fi
+fi
+
+if [[ "$built" = true ]]; then
+	echo "Skipping, already built"
+else
+	flutter build ios-framework --output=build/ios-framework --no-debug --no-profile
+	echo $version > build/.version
+fi
 
 echo "Done"
