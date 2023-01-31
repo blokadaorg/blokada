b531a04b428e3b21b72ed54c218de5815668b2dc
kar <hi@kar.gs>
2023-01-31 11:52:25 +0100

    update sync-common script to build flutter

--- android/sync-common.sh
+++ android/sync-common.sh
@@ -1,14 +1,28 @@
 #!/bin/sh
 
-echo "Syncing common..."
+#
+# This file is part of Blokada.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright © 2023 Blocka AB. All rights reserved.
+#
+# @author Karol Gusak (karol@blocka.net)
+#
+
+echo "Building common..."
 
 cd six-common
-git checkout main
-git pull
 hash=$(git describe --abbrev=4 --always --tags --dirty)
 commit="sync: update six-common to: $hash"
 
 echo $commit
+
+flutter build aar --no-profile
+mkdir -p ../app/six-common/
+cp -r build/host/outputs/repo ../app/six-common/
 
 cd ../
 
