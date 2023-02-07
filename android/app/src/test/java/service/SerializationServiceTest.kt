2bf522bafe9670cf72b12308c1943af604d74750
kar <hi@kar.gs>
2023-02-07 17:16:25 +0100

    update test to pass it

--- android/app/src/test/java/service/SerializationServiceTest.kt
+++ android/app/src/test/java/service/SerializationServiceTest.kt
@@ -12,33 +12,25 @@
 
 package service
 
-import model.HistoryEntry
-import model.HistoryEntryType
-import model.Stats
-import org.junit.Test
+import model.Account
 import org.junit.Assert
+import org.junit.Test
 import java.util.*
 
 class SerializationServiceTest {
     @Test fun basic() {
-        val stats = Stats(
-            allowed = 1,
-            denied = 2,
-            entries = listOf(
-                HistoryEntry(
-                    name = "example.com",
-                    type = HistoryEntryType.passed,
-                    time = Date(),
-                    requests = 1
-                )
-            )
+        val acc = Account(
+            id = "mockedmocked",
+            active_until = Date(1),
+            active = false,
+            type = "mocked",
+            payment_source = null
         )
 
-        val json = SerializationService.serialize(stats)
-        val deserialized = SerializationService.deserialize(json, Stats::class)
+        val json = JsonSerializationService.serialize(acc)
+        val deserialized = JsonSerializationService.deserialize(json, Account::class)
 
-        Assert.assertEquals(1, deserialized.allowed)
-        Assert.assertEquals(2, deserialized.denied)
-        Assert.assertEquals("example.com", deserialized.entries.first().name)
+        Assert.assertEquals(Date(1), deserialized.active_until)
+        Assert.assertEquals("mocked", deserialized.type)
     }
 }
