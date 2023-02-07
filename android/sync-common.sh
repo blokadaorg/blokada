d94ab96bf66feb76d75f8e59232f56cdc28a14f7
kar <hi@kar.gs>
2023-02-07 19:07:13 +0100

    make sync-common pull latest main

--- android/sync-common.sh
+++ android/sync-common.sh
@@ -15,6 +15,8 @@
 echo "Syncing common..."
 
 cd six-common
+git checkout main
+git pull
 hash=$(git describe --abbrev=4 --always --tags --dirty)
 commit="sync: update six-common to: $hash"
 echo $commit
