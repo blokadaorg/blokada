542b42c32f6b348873f0838fa69a25cc7bd31a79
kar <hi@kar.gs>
2023-02-10 12:19:08 +0100

    updates set env script syntax

--- ios/.github/scripts/set-env-from-xcodeproj.sh
+++ ios/.github/scripts/set-env-from-xcodeproj.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 SCHEME="$(xcodebuild -list -json -project IOS.xcodeproj | jq -r '.project.schemes[0]')"
 PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings -project IOS.xcodeproj | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
-echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
+echo "name=PRODUCT_NAME::$PRODUCT_NAME" >> $GITHUB_ENV
 
