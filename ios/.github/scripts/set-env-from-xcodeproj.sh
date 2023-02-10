74ed76c72e48ac3a6e47815e88a579a27bbb475a
kar <hi@kar.gs>
2023-02-10 12:12:39 +0100

    adapt script to our project

--- ios/.github/scripts/set-env-from-xcodeproj.sh
+++ ios/.github/scripts/set-env-from-xcodeproj.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-SCHEME="$(xcodebuild -list -json | jq -r '.project.schemes[0]')"
-PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
+SCHEME="$(xcodebuild -list -json -project IOS.xcodeproj | jq -r '.project.schemes[0]')"
+PRODUCT_NAME="$(xcodebuild -scheme "$SCHEME" -showBuildSettings -project IOS.xcodeproj | grep " PRODUCT_NAME " | sed "s/[ ]*PRODUCT_NAME = //")"
 echo "::set-env name=PRODUCT_NAME::$PRODUCT_NAME"
 
