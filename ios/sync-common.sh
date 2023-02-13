43f260e21b018f19bc8cac8655e6b177b43bf386
kar <hi@kar.gs>
2023-02-13 09:22:14 +0100

    sync: update six-common to: d01c

--- ios/sync-common.sh
+++ ios/sync-common.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+#
+# This file is part of Blokada.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+# Copyright © 2023 Blocka AB. All rights reserved.
+#
+# @author Karol Gusak (karol@blocka.net)
+#
+
 echo "Syncing common..."
 
 cd six-common
@@ -7,13 +19,9 @@ git checkout main
 git pull
 hash=$(git describe --abbrev=4 --always --tags --dirty)
 commit="sync: update six-common to: $hash"
-
 echo $commit
-
-./build.for.ios.sh
-
 cd ../
 
 git commit -am "$commit"
 
-echo "Done"
+echo "Done (run make now)."
