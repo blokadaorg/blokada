1c319a7355d2e0db1f11c32bf094cd7d0161e4ac
kar <hi@kar.gs>
2023-02-27 13:09:09 +0100

    add allowedips to plus tunnel

--- android/app/src/engine/kotlin/engine/WgTunnel.kt
+++ android/app/src/engine/kotlin/engine/WgTunnel.kt
@@ -32,6 +32,15 @@ object WgTunnel {
     private val wgManager = MainApplication.getTunnelManager()
     private val apps = AppRepository
 
+    private val ALLOWED_IPS = listOf(
+        "::/0", "1.0.0.0/8", "2.0.0.0/8", "3.0.0.0/8", "4.0.0.0/6", "8.0.0.0/7", "11.0.0.0/8",
+        "12.0.0.0/6", "16.0.0.0/4", "32.0.0.0/3", "64.0.0.0/2", "128.0.0.0/3", "160.0.0.0/5",
+        "168.0.0.0/6", "172.0.0.0/12", "172.32.0.0/11", "172.64.0.0/10", "172.128.0.0/9",
+        "173.0.0.0/8", "174.0.0.0/7", "176.0.0.0/4", "192.0.0.0/9", "192.128.0.0/11",
+        "192.160.0.0/13", "192.169.0.0/16", "192.170.0.0/15", "192.172.0.0/14", "192.176.0.0/12",
+        "192.192.0.0/10", "193.0.0.0/8", "194.0.0.0/7", "196.0.0.0/6", "200.0.0.0/5", "208.0.0.0/4",
+    )
+
     suspend fun start(privateKey: String, lease: Lease, gateway: Gateway) {
         log.v("Starting WG tunnel for: ${gateway.country}")
 
@@ -51,6 +60,14 @@ object WgTunnel {
 
         log.v("Wg tunnel will use DNS address: $dnsAddress")
 
+        val peerBuilder = Peer.Builder()
+            .setPublicKey(Key.fromBase64(gateway.public_key))
+            .setEndpoint(InetEndpoint.parse("${gateway.ipv4}:51820"))
+
+        ALLOWED_IPS.forEach {
+            peerBuilder.addAllowedIp(InetNetwork.parse(it))
+        }
+
         val c = Config.Builder()
             .setInterface(
                 Interface.Builder()
@@ -61,15 +78,7 @@ object WgTunnel {
                 .excludeApplications(apps.getPackageNamesOfAppsToBypass(forRealTunnel = true))
                 .build()
             )
-            .addPeer(
-                Peer.Builder()
-                .setPublicKey(Key.fromBase64(gateway.public_key))
-                .setEndpoint(InetEndpoint.parse("${gateway.ipv4}:51820"))
-                //.setEndpoint(InetEndpoint.parse("${lease.vip6}:51820"))
-                .addAllowedIp(InetNetwork.parse("0.0.0.0/0"))
-                .addAllowedIp(InetNetwork.parse("::/0"))
-                .build()
-            )
+            .addPeer(peerBuilder.build())
         .build()
 
         val tunnel = wgManager.create("Blokada", c)
