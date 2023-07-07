a902c5f9ebd17663de984a562a715cce95e2ad43
kar <hi@kar.gs>
2023-07-07 11:19:23 +0200

    improve command names for tracing

--- common/lib/command/command.dart
+++ common/lib/command/command.dart
@@ -47,7 +47,7 @@ class CommandStore with Traceable, Dependable, CommandEvents, TraceOrigin {
   @override
   Future<void> onCommand(String command) async {
     _startCommandTimeout(command);
-    await traceAs("cmd", (trace) async {
+    await traceAs(_name(command, null), (trace) async {
       final cmd = CommandName.values.byName(command);
       return await execute(trace, cmd);
     });
@@ -57,7 +57,7 @@ class CommandStore with Traceable, Dependable, CommandEvents, TraceOrigin {
   @override
   Future<void> onCommandWithParam(String command, String p1) async {
     _startCommandTimeout(command);
-    await traceAs("cmd", (trace) async {
+    await traceAs(_name(command, p1), (trace) async {
       final cmd = CommandName.values.byName(command);
       return await execute(trace, cmd, p1: p1);
     });
@@ -67,7 +67,7 @@ class CommandStore with Traceable, Dependable, CommandEvents, TraceOrigin {
   @override
   Future<void> onCommandWithParams(String command, String p1, String p2) async {
     _startCommandTimeout(command);
-    await traceAs("cmd", (trace) async {
+    await traceAs(_name(command, p1), (trace) async {
       final cmd = CommandName.values.byName(command);
       return await execute(trace, cmd, p1: p1, p2: p2);
     });
@@ -84,98 +84,97 @@ class CommandStore with Traceable, Dependable, CommandEvents, TraceOrigin {
     });
   }
 
-  execute(Trace parentTrace, CommandName cmd, {String? p1, String? p2}) async {
-    final name =
-        cmd.name + (p1 != null ? "(${shortString(p1, length: 16)})" : "()");
-    return await traceWith(parentTrace, name, (trace) async {
-      switch (cmd) {
-        case CommandName.restore:
-          await _account.restore(trace, p1!);
-          return await _accountRefresh.syncAccount(trace, _account.account);
-        case CommandName.receipt:
-          await _accountPayment.restoreInBackground(trace, p1!);
-          return await _accountRefresh.syncAccount(trace, _account.account);
-        case CommandName.fetchProducts:
-          return await _accountPayment.fetchProducts(trace);
-        case CommandName.purchase:
-          await _accountPayment.purchase(trace, p1!);
-          return await _accountRefresh.syncAccount(trace, _account.account);
-        case CommandName.restorePayment:
-          // TODO:
-          // Only restore implicitly if current account is not active
-          // TODO: finish ongoing transaction after any success or fail (stop processing)
-          return await _accountPayment.restore(trace);
-        case CommandName.pause:
-          return await _appStart.pauseAppIndefinitely(trace);
-        case CommandName.unpause:
-          return await _appStart.unpauseApp(trace);
-        case CommandName.allow:
-          return await _custom.allow(trace, p1!);
-        case CommandName.deny:
-          return await _custom.deny(trace, p1!);
-        case CommandName.delete:
-          return await _custom.delete(trace, p1!);
-        case CommandName.enableDeck:
-          return await _deck.setEnableDeck(trace, p1!, true);
-        case CommandName.disableDeck:
-          return await _deck.setEnableDeck(trace, p1!, false);
-        case CommandName.enableList:
-          return await _deck.setEnableList(trace, p1!, true);
-        case CommandName.disableList:
-          return await _deck.setEnableList(trace, p1!, false);
-        case CommandName.toggleListByTag:
-          return await _deck.toggleListByTag(trace, p1!, p2!);
-        case CommandName.enableCloud:
-          return await _device.setCloudEnabled(trace, true);
-        case CommandName.disableCloud:
-          return await _device.setCloudEnabled(trace, false);
-        case CommandName.setRetention:
-          return await _device.setRetention(trace, p1!);
-        case CommandName.sortNewest:
-          return await _journal.updateFilter(trace, sortNewestFirst: true);
-        case CommandName.sortCount:
-          return await _journal.updateFilter(trace, sortNewestFirst: false);
-        case CommandName.search:
-          return await _journal.updateFilter(trace, searchQuery: p1!);
-        case CommandName.filter:
-          return await _journal.updateFilter(trace,
-              showOnly: JournalFilterType.values.byName(p1!));
-        case CommandName.filterDevice:
-          return await _journal.updateFilter(trace, deviceName: p1!);
-        case CommandName.newPlus:
-          return await _plus.newPlus(trace, p1!);
-        case CommandName.clearPlus:
-          return await _plus.clearPlus(trace);
-        case CommandName.activatePlus:
-          return await _plus.switchPlus(trace, true);
-        case CommandName.deactivatePlus:
-          return await _plus.switchPlus(trace, false);
-        case CommandName.deleteLease:
-          return await _plusLease.deleteLeaseById(trace, p1!);
-        case CommandName.vpnStatus:
-          return await _plusVpn.setActualStatus(trace, p1!);
-        case CommandName.foreground:
-          return await _stage.setForeground(trace);
-        case CommandName.background:
-          return await _stage.setBackground(trace);
-        case CommandName.route:
-          return await _stage.setRoute(trace, p1!);
-        case CommandName.modalShow:
-          return await _stage.showModal(trace, StageModal.values.byName(p1!));
-        case CommandName.modalShown:
-          return await _stage.modalShown(trace, StageModal.values.byName(p1!));
-        case CommandName.modalDismiss:
-          return await _stage.dismissModal(trace);
-        case CommandName.modalDismissed:
-          return await _stage.modalDismissed(trace);
-        case CommandName.remoteNotification:
-          return await _accountRefresh.onRemoteNotification(trace);
-        case CommandName.appleNotificationToken:
-          return await _notification.saveAppleToken(trace, p1!);
-        case CommandName.error:
-          return await platformError(trace, p1!);
-      }
-    });
+  _name(String cmd, String? p1) =>
+      cmd + (p1 != null ? "(${shortString(p1, length: 16)})" : "()");
+
+  execute(Trace trace, CommandName cmd, {String? p1, String? p2}) async {
+    switch (cmd) {
+      case CommandName.restore:
+        await _account.restore(trace, p1!);
+        return await _accountRefresh.syncAccount(trace, _account.account);
+      case CommandName.receipt:
+        await _accountPayment.restoreInBackground(trace, p1!);
+        return await _accountRefresh.syncAccount(trace, _account.account);
+      case CommandName.fetchProducts:
+        return await _accountPayment.fetchProducts(trace);
+      case CommandName.purchase:
+        await _accountPayment.purchase(trace, p1!);
+        return await _accountRefresh.syncAccount(trace, _account.account);
+      case CommandName.restorePayment:
+        // TODO:
+        // Only restore implicitly if current account is not active
+        // TODO: finish ongoing transaction after any success or fail (stop processing)
+        return await _accountPayment.restore(trace);
+      case CommandName.pause:
+        return await _appStart.pauseAppIndefinitely(trace);
+      case CommandName.unpause:
+        return await _appStart.unpauseApp(trace);
+      case CommandName.allow:
+        return await _custom.allow(trace, p1!);
+      case CommandName.deny:
+        return await _custom.deny(trace, p1!);
+      case CommandName.delete:
+        return await _custom.delete(trace, p1!);
+      case CommandName.enableDeck:
+        return await _deck.setEnableDeck(trace, p1!, true);
+      case CommandName.disableDeck:
+        return await _deck.setEnableDeck(trace, p1!, false);
+      case CommandName.enableList:
+        return await _deck.setEnableList(trace, p1!, true);
+      case CommandName.disableList:
+        return await _deck.setEnableList(trace, p1!, false);
+      case CommandName.toggleListByTag:
+        return await _deck.toggleListByTag(trace, p1!, p2!);
+      case CommandName.enableCloud:
+        return await _device.setCloudEnabled(trace, true);
+      case CommandName.disableCloud:
+        return await _device.setCloudEnabled(trace, false);
+      case CommandName.setRetention:
+        return await _device.setRetention(trace, p1!);
+      case CommandName.sortNewest:
+        return await _journal.updateFilter(trace, sortNewestFirst: true);
+      case CommandName.sortCount:
+        return await _journal.updateFilter(trace, sortNewestFirst: false);
+      case CommandName.search:
+        return await _journal.updateFilter(trace, searchQuery: p1!);
+      case CommandName.filter:
+        return await _journal.updateFilter(trace,
+            showOnly: JournalFilterType.values.byName(p1!));
+      case CommandName.filterDevice:
+        return await _journal.updateFilter(trace, deviceName: p1!);
+      case CommandName.newPlus:
+        return await _plus.newPlus(trace, p1!);
+      case CommandName.clearPlus:
+        return await _plus.clearPlus(trace);
+      case CommandName.activatePlus:
+        return await _plus.switchPlus(trace, true);
+      case CommandName.deactivatePlus:
+        return await _plus.switchPlus(trace, false);
+      case CommandName.deleteLease:
+        return await _plusLease.deleteLeaseById(trace, p1!);
+      case CommandName.vpnStatus:
+        return await _plusVpn.setActualStatus(trace, p1!);
+      case CommandName.foreground:
+        return await _stage.setForeground(trace);
+      case CommandName.background:
+        return await _stage.setBackground(trace);
+      case CommandName.route:
+        return await _stage.setRoute(trace, p1!);
+      case CommandName.modalShow:
+        return await _stage.showModal(trace, StageModal.values.byName(p1!));
+      case CommandName.modalShown:
+        return await _stage.modalShown(trace, StageModal.values.byName(p1!));
+      case CommandName.modalDismiss:
+        return await _stage.dismissModal(trace);
+      case CommandName.modalDismissed:
+        return await _stage.modalDismissed(trace);
+      case CommandName.remoteNotification:
+        return await _accountRefresh.onRemoteNotification(trace);
+      case CommandName.appleNotificationToken:
+        return await _notification.saveAppleToken(trace, p1!);
+      case CommandName.error:
+        return await platformError(trace, p1!);
+    }
   }
 
   platformError(Trace parentTrace, String error) async {
@@ -187,9 +186,6 @@ class CommandStore with Traceable, Dependable, CommandEvents, TraceOrigin {
   }
 
   _onCommandTimer(String command) {
-    // This command is just super slow because of StoreKit
-    if (command == "purchase") return;
-
     try {
       _timer.addHandler("command:$command", (trace) async {
         // This will just show up in tracing
@@ -199,13 +195,18 @@ class CommandStore with Traceable, Dependable, CommandEvents, TraceOrigin {
   }
 
   _startCommandTimeout(String command) {
+    // This command is just super slow because of StoreKit
+    if (command == "purchase") return;
+
     _onCommandTimer(command);
     _timer.set(
         "command:$command", DateTime.now().add(cfg.plusVpnCommandTimeout));
   }
 
   _stopCommandTimeout(String command) {
-    //_ongoingCommand = "";
+    // This command is just super slow because of StoreKit
+    if (command == "purchase") return;
+
     _timer.unset("command:$command");
   }
 }
