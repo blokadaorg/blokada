086814f011218c82f97c5991a6802e2800ac7e84
kar <hi@kar.gs>
2023-07-12 09:02:45 +0200

    generate android files in separate namespaces
    There is some FlutterError redeclaration

--- common/sync-generated-files.sh
+++ common/sync-generated-files.sh
@@ -10,10 +10,11 @@ mkdir -p libgen/android
 for file in libgen/schema/*.dart
 do
     name=$(basename "$file" | cut -f 1 -d '.')
+    name_lower=$(echo "$name" | tr '[:upper:]' '[:lower:]')
     dartPath=$(echo "$name" | sed 's/\([A-Z]\)/\/\1/g')
     dartPath=$(echo $dartPath | tr '[:upper:]' '[:lower:]')
     echo $dartPath
-    dart run pigeon --input $file --dart_out lib$dartPath/channel.pg.dart --swift_out libgen/ios/$name.swift --kotlin_out libgen/android/$name.kt --kotlin_package "channel"
+    dart run pigeon --input $file --dart_out lib$dartPath/channel.pg.dart --swift_out libgen/ios/$name.swift --kotlin_out libgen/android/$name.kt --kotlin_package "channel.${name_lower}"
 done
 
 echo "Running flutter build_runner..."
