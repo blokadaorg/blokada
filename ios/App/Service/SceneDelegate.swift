2619bc8f10aa69c1fb17fa4b0965a0f569558dc3
kar <hi@kar.gs>
2024-05-03 11:26:21 +0200

    Family MVP2 Preview 3

--- ios/App/Service/SceneDelegate.swift
+++ ios/App/Service/SceneDelegate.swift
@@ -96,7 +96,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        // This may occur due to temporary interruptions (ex. an incoming phone call).`
 
         // Close the kb when leaving to bg
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
