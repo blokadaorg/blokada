8b95b9630a13303eabe83a3687b4b38eb0e82081
kar <hi@kar.gs>
2024-10-23 14:30:24 +0200

    use key emoji to denote censored param

--- common/lib/logger/logger.dart
+++ common/lib/logger/logger.dart
@@ -65,9 +65,9 @@ class Log {
           var bytes = utf8.encode(censored);
           var digest = md5.convert(bytes);
 
-          var value = "*${digest.toString()}*";
+          var value = "🔑 ${digest.toString()}";
           if (param != censored) {
-            value = "$param?*$digest*";
+            value = "$param? 🔑 $digest";
           }
 
           lines.add("💡 $tag 🔍 $key = $value");
