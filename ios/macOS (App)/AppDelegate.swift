8cdb221d936255c5529cb9f3f5e385ece9705ca9
kar <karolgusak@gmail.com>
2025-04-25 11:16:59 +0200

    ios: add blockaweb extension template for Safari

---
+++ ios/macOS (App)/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  This file is part of Blokada.
+//
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+//  Copyright © 2025 Blocka AB. All rights reserved.
+//
+//  @author Karol Gusak
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
