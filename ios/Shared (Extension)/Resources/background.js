50f4f326717d2d47061b49d2251bf0423429b9f2
kar <karolgusak@gmail.com>
2025-04-29 09:01:15 +0200

    blockaweb: cleanup project target setup

--- ios/Shared (Extension)/Resources/background.js
+++
@@ -1,6 +0,0 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
-});
