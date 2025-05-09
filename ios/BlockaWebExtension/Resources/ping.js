a4b404957b827beb7ef9c32c9f4a6e6dbf702ef6
kar <karolgusak@gmail.com>
2025-05-09 17:47:42 +0200

    Revert "blockaweb: fix ping"
    This reverts commit 09ccd3e54dd9b9d912a50e3a3374dd599ebe8889.

--- ios/BlockaWebExtension/Resources/ping.js
+++ ios/BlockaWebExtension/Resources/ping.js
@@ -1,13 +1,8 @@
 // ping.js: Relay a ping message to the page context on go.blokada.org
 (async function () {
   async function ping() {
-    if (typeof browser !== 'undefined' && browser.runtime && browser.runtime.sendMessage) {
-      await browser.runtime.sendMessage({ message: 'status' });
-      window.postMessage({ type: 'ping', url: window.location.href }, '*');
-    } else {
-      // Retry ping after a short delay if browser API isn't ready
-      setTimeout(ping, 300);
-    }
+    window.postMessage({ type: 'ping', url: window.location.href }, '*');
+    await browser.runtime.sendMessage({ message: 'status' });
   }
   console.log('ping.js: posting ping message');
   if (document.readyState === 'complete' || document.readyState === 'interactive') {
