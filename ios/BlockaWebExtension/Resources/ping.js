94a0442aa40b425790cfbcf2be827fff8d7debae
Johnny Bergström <johnny@klaudify.se>
2025-05-09 12:40:35 +0200

    blockaweb: fix ping

--- ios/BlockaWebExtension/Resources/ping.js
+++ ios/BlockaWebExtension/Resources/ping.js
@@ -1,12 +1,15 @@
 // ping.js: Relay a ping message to the page context on go.blokada.org
-(function () {
-  function ping() {
+(async function () {
+  async function ping() {
     window.postMessage({ type: 'ping', url: window.location.href }, '*');
+    await browser.runtime.sendMessage({ message: 'status' });
   }
   console.log('ping.js: posting ping message');
   if (document.readyState === 'complete' || document.readyState === 'interactive') {
-    ping();
+    await ping();
   } else {
-    document.addEventListener('DOMContentLoaded', ping);
+    document.addEventListener('DOMContentLoaded', () => {
+      ping();
+    });
   }
 })();
