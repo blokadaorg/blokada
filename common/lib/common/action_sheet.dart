2714a48ff1bb4c3cb916b4be5ec943bec7ea2621
kar <karolgusak@gmail.com>
2025-06-18 13:09:03 +0200

    v6: use new pause dialog strings

--- common/lib/common/action_sheet.dart
+++ common/lib/common/action_sheet.dart
@@ -2,12 +2,11 @@ import 'package:common/common/widget/theme.dart';
 import 'package:common/core/core.dart';
 import 'package:flutter/cupertino.dart';
 
-void showPauseActionSheet(BuildContext context,
-    {required Function(Duration?) onSelected}) {
+void showPauseActionSheet(BuildContext context, {required Function(Duration?) onSelected}) {
   showCupertinoModalPopup<void>(
     context: context,
     builder: (BuildContext context) => CupertinoActionSheet(
-      title: Text("home status active".i18n),
+      title: Text("home power pause status active".i18n),
       message: Text("home power off menu header".i18n),
       actions: <CupertinoActionSheetAction>[
         CupertinoActionSheetAction(
@@ -16,7 +15,7 @@ void showPauseActionSheet(BuildContext context,
             onSelected(const Duration(minutes: 5));
             Navigator.pop(context);
           },
-          child: Text("home power action pause".i18n,
+          child: Text("home power action pause five".i18n,
               style: TextStyle(color: context.theme.cloud)),
         ),
         CupertinoActionSheetAction(
@@ -25,14 +24,13 @@ void showPauseActionSheet(BuildContext context,
             onSelected(null);
             Navigator.pop(context);
           },
-          child: Text("home power action turn off".i18n),
+          child: Text("home power action off all".i18n),
         ),
         CupertinoActionSheetAction(
           onPressed: () {
             Navigator.pop(context);
           },
-          child: Text("universal action cancel".i18n,
-              style: TextStyle(color: context.theme.cloud)),
+          child: Text("universal action cancel".i18n, style: TextStyle(color: context.theme.cloud)),
         ),
       ],
     ),
